8b8e97a490424e391c8c7f32c9342b7cae7cfa52
mj0107 <alswns1285@gmail.com>
2022-05-09 03:26:05 +0900

    줄 세우기(변수명 수정)

--- BOJ/Graph/2252.js
+++ BOJ/Graph/2252.js
@@ -12,20 +12,20 @@ input.map(item => {
 
 function solution(N, M, testInputCase) {
   let graph = Array.from({length: N + 1}, () => [0, 0]);
-  let inDegrees = Array(N + 1).fill(0);
+  let indegrees = Array(N + 1).fill(0);
   let result = [];
 
   let queue = [];
 
   for(const [from, to] of testInputCase) {
     graph[from].push(to); // from과 연결된 to 노드 push
-    inDegrees[to]++; // to 노드와 연결된 from의 갯수만큼 + 1
+    indegrees[to]++; // to 노드와 연결된 from의 갯수만큼 + 1
   }
 
   // 노드와 연결된 from 노드의 개수가 0이라면,
   // 시작노드로 설정해서 queue에 삽입
   for(let i=1; i<=N; i++) {
-    if(inDegrees[i] === 0) queue.push(i);
+    if(indegrees[i] === 0) queue.push(i);
   }
 
   while(queue.length) {
@@ -35,10 +35,10 @@ function solution(N, M, testInputCase) {
     // from 노드로 부터 연결된 모든 노드를 순회
     for(const next of graph[from]) {
       // from 노드를 queue에서 뺐기 때문에, from 노드와 연결된 노드들의 inDegress - 1
-      inDegrees[next]--;
+      indegrees[next]--;
       // 하나 빼준 값이 0이라면, 이전에 연결된 노드가 없다는 것이므로,
       // 시작 노드로 설정
-      if(inDegrees[next] === 0) queue.push(next);
+      if(indegrees[next] === 0) queue.push(next);
     }
   }
 
