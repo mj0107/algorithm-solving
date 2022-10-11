59bf3939b37b94f1d93399325000e70a52282b3c
mj0107 <alswns1285@gmail.com>
2022-10-11 19:53:52 +0900

    오르막 수

--- BOJ/DP/11057.js
+++ BOJ/DP/11057.js
@@ -7,19 +7,23 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const N = +input.shift();
 
 function solution(N) {
-  let dp = [[], new Array(10).fill(1)];
+  let dp = Array.from({ length: N + 1 }, () => new Array(10).fill(0));
   const DIVISOR = 1e4 + 7;
 
-  for (let i = 2; i <= N; i++) {
-    dp[i] = [];
-    dp[i][0] = dp[i - 1][0] % DIVISOR;
+  dp[1].fill(1);
+  for (let i = 1; i <= N; i++) {
+    dp[i][0] = 1;
+  }
 
+  for (let i = 1; i <= N; i++) {
     for (let j = 1; j < 10; j++) {
-      dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % DIVISOR;
+      dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % DIVISOR;
     }
   }
 
-  console.log(dp[N].reduce((acc, cur) => acc + cur, 0) % DIVISOR);
+  const result = dp[N].reduce((acc, cur) => acc + cur) % DIVISOR;
+
+  console.log(result);
 }
 
 solution(N);
