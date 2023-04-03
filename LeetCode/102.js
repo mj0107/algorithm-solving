a61f06a24d71e9e93329c7417fecd9141ffd27bf
mj0107 <alswns1285@gmail.com>
2023-04-03 23:56:32 +0900

    Binary Tree Level Order Traversal
    - 나중에 고쳐야함

---
+++ LeetCode/102.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ var levelOrder = function(root) {
+  if(root === null) return [];
+  
+  let result = [];
+  let queue = [];
+  queue.push(root);
+
+  while(queue.length) {
+      let level = [];
+
+      for(let i=0; i<queue.length; i+=1) {
+          let node = queue.shift();
+
+          level.push(node.val);
+          node.left ? queue.push(node.left) : null;
+          node.right ? queue.push(node.right) : null;
+      }
+
+      result.push(level);
+  }
+
+  return result;
+};
