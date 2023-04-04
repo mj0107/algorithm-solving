de00c73686c51b0cb2d72132ef47fde787581344
mj0107 <alswns1285@gmail.com>
2023-04-04 21:44:56 +0900

    Binary Search

---
+++ LeetCode/704.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid = 0;
+
+  while(left <= right) {
+      mid = Math.floor((left + right) / 2);
+      
+      if(nums[mid] < target) {
+          left = mid + 1;
+      }
+      if(nums[mid] > target) {
+          right = mid - 1;
+      }
+      if(nums[mid] === target) {
+          return mid;
+      }
+  }
+
+  return -1;
+};
