12093fb3b856c1fd5e48df1e0bc02947172477d2
김민준 <alswns1285@gmail.com>
2024-11-05 23:16:58 +0900

    최소 힙

---
+++ BOJ/Math/1927.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'BOJ/input.txt';
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.trim());
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp(this.heap.length - 1);
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown(0);
+    return min;
+  }
+
+  heapifyUp(index) {
+    const parentIndex = Math.floor((index - 1) / 2);
+
+    if (parentIndex >= 0 && this.heap[parentIndex] > this.heap[index]) {
+      this.swap(index, parentIndex);
+      this.heapifyUp(parentIndex);
+    }
+  }
+
+  heapifyDown(index) {
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = 2 * index + 2;
+    let smallestIndex = index;
+
+    if (
+      leftChildIndex < this.heap.length &&
+      this.heap[leftChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = leftChildIndex;
+    }
+
+    if (
+      rightChildIndex < this.heap.length &&
+      this.heap[rightChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = rightChildIndex;
+    }
+
+    if (smallestIndex !== index) {
+      this.swap(index, smallestIndex);
+      this.heapifyDown(smallestIndex);
+    }
+  }
+
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+}
+
+const N = +input.shift();
+const commandList = input.map(Number);
+
+function solution() {
+  const minHeap = new MinHeap();
+
+  const result = [];
+  for (const command of commandList) {
+    if (command === 0) {
+      if (minHeap.isEmpty()) {
+        result.push(0);
+        continue;
+      }
+      result.push(minHeap.extractMin());
+    } else if (!isNaN(command)) {
+      minHeap.insert(command);
+    }
+  }
+
+  console.log(result.join('\n'));
+}
+
+solution();
